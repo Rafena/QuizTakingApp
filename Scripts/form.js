289eef2d9604b61cd7f768952d9283e2b826dd16
Rafael Santana <santana.rafael27@gmail.com>
2018-12-16 21:31:40 -0500

    Live preview worked

--- Scripts/form.js
+++ Scripts/form.js
@@ -3,13 +3,12 @@
 
 
 
-var subjects_list =["Binary Trees", "Linkced Lists","Matematical Induction"];
-
+var subjects_list =["Unit 1: Introduction","Unit 2: Array-Based Lists","Unit 3: Linked Lists","Unit 4: Skiplists","Unit 5: Hash Tables","Unit 6: Recursion","Unit 7: Binary Trees","Unit 8: Scapegoat Trees","Unit 9: Red–Black Trees","Unit 10: Heaps","Unit 11:Sorting Algorithms","Unit 12: Graphs","Unit 13: Binary Trie"];
+var question;
  //var  subjects_element = document.getElementById('qexampleFormControlSelect1') ;
 
 
 
-alert("Eco");
 $(document).ready(function(){
 
 
@@ -26,7 +25,17 @@ $("select").append( $("<option />").val(subjects_list[i]).text(subjects_list[i])
 
 
 
+function when_typed(){
+  question= $('#questionid').val();
 
+
+$('#qid').text(question);
+
+//  alert(question);
+
+
+
+}
 
 
 
